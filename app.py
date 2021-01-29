170fa0561d15bf16b3fdf8d1aac18a7f01fdf39f
Retsleds <70805511+Retsleds@users.noreply.github.com>
2021-01-29 10:03:04 +0800

    new

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, Response
 import settings
 
 app = Flask(__name__)
@@ -7,7 +7,15 @@ app.config.from_object(settings)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Flask world!'
+    return '我想放假！！！！!'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        return Response(f'Register Successfully! \nA user whose name is [{username}] wanna register. His password is [{password}]')
+    return render_template('user_registeration.html')
 
 
 if __name__ == '__main__':
